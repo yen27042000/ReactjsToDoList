075f22b91d73a47039e9f6bdc7b415df1c4b912c
yen27042000 <yen6238@gmail.com>
2021-06-30 22:32:08 +0700

    Buoc-1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { taskName: '' ,
+                   task:['task1','task2','task3']};
+  }
+ ///hàm lấy dữ liệu từ textbox
+  myTaskChangeHandler=(event)=>{
+    this.setState({taskName: event.target.value});
+    //console.log('myTaskChangeHandler',event.target.value);
+  };
+ //Hàm click để xuất dữ liệu
+  addTask(){
+    console.log('addtaskh',this.state.taskName);
+    if(this.state.taskName===''){
+        alert('Vui lòng nhập dữ liệu')
+    }else{
+      // Đẩy vào mảng 
+      this.state.task.push(this.state.taskName);
+      //câu lệnh set text box về rỗng khi click chọn xong
+       this.setState({taskName: ''});
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+          <br />
+          To do list
+          <br />
+          <div className="aligned">
+            <img
+              src='./assets/icons8_insert_equation_50px.png'
+              alt='Add Task'
+              width='25px'
+              style={{cursor:'pointer', paddingRight:'10px'}}
+              title='Bấm để task'
+              onClick={() => this.addTask()}
+            />
+            <input type='text'
+              value={this.state.taskName}
+              onChange={this.myTaskChangeHandler}/>
+
+          </div>
+          <ul>
+              {this.state.task.map((value,index)=>{
+                return <li key={index} >{value}</li>
+              })}
+            </ul>
+      </div>
+    )
+  };
+
 }
-
 export default App;
+
